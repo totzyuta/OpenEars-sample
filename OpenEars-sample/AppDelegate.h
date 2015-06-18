e7db2bf81de32fcd06758efbab596904bed10e5a
totzYuta <yuta.totz@gmail.com>
2015-06-18 18:16:14 +0900

    Initial Commit

---
+++ OpenEars-sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenEars-sample
+//
+//  Created by yutatotz on 6/18/15.
+//  Copyright (c) 2015 yutatotz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenEars-sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenEars-sample
+//
+//  Created by yutatotz on 6/18/15.
+//  Copyright (c) 2015 yutatotz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
